f7c32bace699f8e4497704b3e695a20082c355af
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-09-29 18:22:54 +0100

    Use log4js and Use Strict Transport Security

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
 const logger = require('morgan')
+const log4js = require('log4js')
+log4js.configure({
+  appenders: {
+    console: { type: 'console' }
+  },
+  categories: {
+    another: { appenders: ['console'], level: 'error' },
+    default: { appenders: ['console'], level: 'trace' }
+  }
+});
+
+const log4jsLogger = log4js.getLogger();
+
+
 const redis = require('redis');
 let client
 if (process.env.REDIS_URL) {
@@ -12,13 +26,17 @@ if (process.env.REDIS_URL) {
 }
 
 client.on('connect', () => {
-  console.log('connected');
+  log4jsLogger.info('connected');
 });
 
 app.use(logger('dev'));
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+function requestHandler(req, res) {
+  res.setHeader('Strict-Transport-Security', 'max-age=630720; includeSubDomains; preload');
+}
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
@@ -33,9 +51,11 @@ require('./server/routes/order-routes')(app);
 
 app.listen(PORT, (err) => {
   if (err) {
-    // logger
+    log4jsLogger.error(`Error starting the app:${err}`)
   }
-  console.log(`The server is running on localhost PORT: ${PORT}`);
+  log4jsLogger.info(`The server is running on localhost PORT: ${PORT}`);
 });
-
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome',
+}));
 module.exports = app;
