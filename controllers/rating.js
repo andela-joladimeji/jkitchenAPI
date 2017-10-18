a09a5bb894a88033c5abe4274040b0fa2bc9edb3
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-10-18 11:16:17 +0100

    remove commented out code in master ratings controller

--- controllers/rating.js
+++ controllers/rating.js
@@ -89,39 +89,4 @@ module.exports = {
         res.status(500).send(error)
       })
   }
-  // incrementMealRate(req, res) {
-  //   Rating.findById(req.params.rateId)
-  //     .then(rate => {
-  //       return rate.increment(
-  //         'ratings': {by: numberToIncrementBy}
-  //       )
-  //   })
-  //     .then({
-
-  //   })
-  // },
-  // decrementMealRate(req, res) {
-  //   Rating.findById(req.params.rateId)
-  //     .then(rate => {
-  //       return rate.increment(
-  //         'ratings': {by: numberToDecrementBy}
-  //       )
-  //   })
-  //   .then((rate) =>{
-
-  //   })
-  // },
-  // updateRate(req, res) {
-  //   Rating.findById(req.params.rateId)
-  //     .then(rate => {
-  //       return rate.update({
-  //         ratings: req.body.ratings
-  //       })
-  //     .then((updatedRate) => {
-  //       res.status(200)
-  //     }) 
-  //   })
-  //   .catch((error)=>{
-  //     res.status(500).send(error)
-  //   })
 }
