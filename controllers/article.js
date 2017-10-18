9ef615f1f349004dddaae491109a36a6ca04c409
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-10-18 10:23:04 +0100

    Add validator middleware for articles

--- controllers/article.js
+++ controllers/article.js
@@ -4,6 +4,13 @@ const Comment = require('../models').Comment;
 
 module.exports = {
   // Only admin can create and update Article
+   /**
+  * @description - Creates a new article
+  * @param {object} request - request object containing the article title, price,available_quantity,image,
+  * description received from the client
+  * @param {object} response - response object served to the client
+  * @returns {json} article - new article created
+  */
   create(req, res) {
     const newArticle = {
       title: req.body.title,
@@ -12,6 +19,8 @@ module.exports = {
       imageURL: req.body.imageURL,
       userId: req.params.userId
     };
+    // Type is either revision or article. 
+    // a user can create a revision of an article
     if (req.body.type) {
       newArticle.type = req.body.type;     
     }
@@ -22,7 +31,12 @@ module.exports = {
         res.status(500).send(error);
       });
   },
-
+  /**
+  * @description - Fetches all articles
+  * @param {object} request - request object received from the client
+  * @param {object} response - response object served to the client
+  * @returns {json} articles - articles fetched
+  */
   list(req, res) {
     return Article
       .findAll()
@@ -30,6 +44,12 @@ module.exports = {
       .catch((error) => res.status(500).send(error));
   },
 
+   /**
+  * @description - Fetches an article
+  * @param {object} request - request object received from the client
+  * @param {object} response - response object served to the client
+  * @returns {json} article - fetched article
+  */
   getOne(req, res) {
     return Article
       .findById(req.params.articleId)
@@ -46,6 +66,12 @@ module.exports = {
       });
   },
 
+   /**
+  * @description - Updates article details
+  * @param {object} request - request object received from the client
+  * @param {object} response - response object served to the client
+  * @returns {json} article - updated article details
+  */
   update(req, res) {
     return Article
       .findById(req.params.articleId)
@@ -67,7 +93,12 @@ module.exports = {
       })
       .catch((error) => res.status(500).send(error));
   },
-
+   /**
+  * @description - Deletes an article
+  * @param {object} request - request object received from the client
+  * @param {object} response - response object served to the client
+  * @returns {json} message, response or error
+  */
   destroy(req, res) {
     return Article
       .findById(req.params.articleId)
@@ -79,17 +110,58 @@ module.exports = {
         }
         return article
           .destroy()
-          .then(() => res.status(200).send({message: 'Article deleted.'}))
+          .then(() => res.status(200).send({message: 'Article deleted.'}));
       })
-      .catch((error) => res.status(500).send(error));
+      .catch(error => res.status(500).send(error));
   },
+  validate(req, res, next) {
+    req.checkBody('title', 'Please enter the title of your article.').notEmpty().isLength({ max: 30 });
+    req.checkBody('content', 'You must enter the content of the article and allows only alphabets').notEmpty().isAlpha();
+    req.checkBody('excerpt', 'Enter a short summary of your article. It must not be less than 1000 characters.').optional().isLength({ max: 1000 });
+    req.checkBody('imageURL', 'You must enter a valid url for your image.').optional().isURL();
 
-  hasAuthorization(req, res, next){
-    if (req.blog.user.id !== req.user.id) {
-        return res.status(403).send({message: 'User is not authorized'
-        });
+    const validatorErrors = req.validationErrors();
+    if (validatorErrors) {
+      const response = [];
+      validatorErrors.forEach(function(err) {
+        response.push(err.msg);
+      });
+      return res.status(422).json({ message: response});
+    } else {
+      return next();
+    }
+  },
+  validateBeforeUpdate(req, res, next) {
+    req.checkBody('title', 'Please enter the title of your article.').optional().isLength({ max: 30 });
+    req.checkBody('content', 'You must enter the content of the article and allows only alphabets').optional().isAlpha();
+    req.checkBody('excerpt', 'Enter a short summary of your article. It must not be less than 1000 characters.').optional().isLength({ max: 1000 });
+    req.checkBody('imageURL', 'You must enter a valid url for your image.').optional().isURL();
+
+    const validatorErrors = req.validationErrors();
+    if (validatorErrors) {
+      const response = [];
+      validatorErrors.forEach(function(err) {
+        response.push(err.msg);
+      });
+      return res.status(422).json({ message: response});
+    } else {
+      return next();
+    }
+  },
+  sanitizer(req, res, next) {
+    req.checkBody('name').escape();
+    return next();
+  },
+  desanitizer(req, res, next) {
+    req.checkBody('name').unescape();
+    return next();
+  },
+  hasAuthorization(req, res, next) {
+    if (req.article.user.id !== req.user.id) {
+      return res.status(403).send({ message: 'User is not authorized',
+      });
     }
     next();
-  }
+  },
 };
 
--- routes/article-routes.js
+++ routes/article-routes.js
@@ -1,11 +1,11 @@
-const articleController = require('../controllers/article')
+const articleController = require('../controllers/article');
 
 module.exports = (app) => {
   app.route('/api/v1/users/:userId([0-9]+)/articles')
-    .post(articleController.hasAuthorization, articleController.create)
-    .get(articleController.list)
+    .post(articleController.hasAuthorization, articleController.validate, articleController.sanitizer)
+    .get(articleController.list);
   app.route('/api/v1/users/:userId([0-9]+)/articles/:articleId')
     .get(articleController.getOne)
-    .put(articleController.hasAuthorization, articleController.update)
-    .delete(articleController.hasAuthorization, articleController.destroy)
-}
+    .put(articleController.hasAuthorization, articleController.validateBeforeUpdate, articleController.update)
+    .delete(articleController.hasAuthorization, articleController.destroy);
+};
