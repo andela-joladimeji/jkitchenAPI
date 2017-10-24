6935b24bef37e738fa8b40f3158f70267d7b2af3
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-10-24 13:00:10 +0100

    Properly validate the article routes

--- routes/article-routes.js
+++ routes/article-routes.js
@@ -1,11 +1,12 @@
 const articleController = require('../controllers/article');
+const auth = require('../middleware/authentication');
 
 module.exports = (app) => {
   app.route('/api/v1/users/:userId([0-9]+)/articles')
-    .post(articleController.hasAuthorization, articleController.validate, articleController.sanitizer)
+    .post(auth.verifyToken, auth.verifyAdminAccess, articleController.validate, articleController.create)
     .get(articleController.list);
   app.route('/api/v1/users/:userId([0-9]+)/articles/:articleId')
     .get(articleController.getOne)
-    .put(articleController.hasAuthorization, articleController.validateBeforeUpdate, articleController.update)
-    .delete(articleController.hasAuthorization, articleController.destroy);
+    .put(auth.verifyToken, auth.verifyAdminAccess, articleController.validateBeforeUpdate, articleController.update)
+    .delete(auth.verifyToken, auth.verifyAdminAccess, articleController.destroy);
 };
