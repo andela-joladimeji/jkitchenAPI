a2647500bdc3bc89529facafe5fd82ef976ddc2c
Jumoke Oladimeji <jumoke5ng@yahoo.com>
2017-10-24 12:58:01 +0100

    Add validate function to routes

--- routes/meal-routes.js
+++ routes/meal-routes.js
@@ -1,13 +1,15 @@
 const mealController = require('../controllers/meal')
+const auth = require('../middleware/authentication');
 
 module.exports = (app) => {
-  app.route('/api/v1/meals') 
-    .post(mealController.validate, mealController.create)
+  app.route('/api/v1/users/:userId([0-9]+)/meals') 
+    .post(auth.verifyToken, auth.verifyAdminAccess, mealController.validate, mealController.create)
     .get(mealController.list);
-  app.route('/api/v1/meals/:mealId([0-9]+)')
+  app.route('/api/v1/users/:userId([0-9]+)/meals/:mealId([0-9]+)')
     .get(mealController.getOne)
-    .put(mealController.update, mealController.validateBeforeUpdate)
-    .delete(mealController.destroy);
-  app.route('/api/v1/popularMeals')
+    .put(auth.verifyToken, auth.verifyAdminAccess, mealController.validateBeforeUpdate, mealController.update)
+    .delete(auth.verifyToken, auth.verifyAdminAccess, mealController.destroy);
+  app.route('/api/v1/meals/popularMeals')
     .get(mealController.getMostPopularMeals);
 }
+
